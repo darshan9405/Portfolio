785fec72c284fecc2b112623c3622313f8bcc1a5
darshan9405 <narendarbhandari9188@gmail.com>
2022-07-25 07:14:50 +0530

    good start

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import {Fragment} from 'react'
+function App() {
+  return (
+    <Fragment className="App">
+      <p>Hello</p>
+    </Fragment>
+  );
+}
+
+export default App;
