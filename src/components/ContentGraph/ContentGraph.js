e7b88cc677d509d34ddc653213c1aa277a01b7fe
darshan9405 <narendarbhandari9188@gmail.com>
2022-07-28 09:35:19 +0530

    add projects component

--- src/components/ContentGraph/ContentGraph.js
+++ src/components/ContentGraph/ContentGraph.js
@@ -1,37 +1,41 @@
 import classes from './ContentGraph.module.css'
 const ContentGraph = () => {
   return (
-    <div className={classes.growthCard}>
-      <div className={classes.graphDiv}>
-        <div className={classes.graph}>
-          <span className={classes.nonblink}>•</span>
+    <div className={classes.contentGraph}>
+      <h2 style={{fontSize:'2.5rem'}}>Journey</h2>
+      <br />
+      <br />
+      <div className={classes.growthCard}>
+        <div className={classes.graphDiv}>
+          <div className={classes.graph}>
+            <span className={classes.nonblink}>•</span>
+          </div>
+          <div className={classes.line}></div>
+          <div className={classes.graph}>
+            <span className={classes.nonblink}>•</span>
+          </div>
+          <div className={classes.line}></div>
+          <div className={classes.graph}>
+            <span className={classes.blink}>•</span>
+          </div>
         </div>
-        <div className={classes.line}></div>
-        <div className={classes.graph}>
-          <span className={classes.nonblink}>•</span>
-        </div>
-        <div className={classes.line}></div>
-        <div className={classes.graph}>
-          <span className={classes.blink}>•</span>
-        </div>
-      </div>
-      <div className={classes.infDiv}>
-        <div className={classes.card}>Born 12/12/2002</div>
-        <br />
-        <br />
-        <div className={classes.card}>
-          <p style={{ margin: '0' }}>
-            Schooling and Junior College, from Ichalkaranji.
-          </p>
-        </div>
-        <br />
-        <br />
-        <div className={classes.card}>
-          <p style={{ margin: '0' }}>
-            Bachelor of Engineering,Information Technology
-            Pune Institute of Computer Technology 
-            2021-Present Pune -India
-          </p>
+        <div className={classes.infDiv}>
+          <div className={classes.card}>Born 12/12/2002</div>
+          <br />
+          <br />
+          <div className={classes.card}>
+            <p style={{ margin: '0' }}>
+              Schooling and Junior College, from Ichalkaranji.
+            </p>
+          </div>
+          <br />
+          <br />
+          <div className={classes.card}>
+            <p style={{ margin: '0' }}>
+              Bachelor of Engineering,Information Technology Pune Institute of
+              Computer Technology 2021-Present Pune -India
+            </p>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,16 +1,58 @@
 import classes from './Projects.module.css'
+import { Button } from 'react-bootstrap'
 const Projects = () => {
   return (
     <div className={classes.projects}>
-      <h3>Projects</h3>
+      <h2 style={{ fontSize: '2.5rem' }}>Projects</h2>
+      <br />
+      <br />
       <div className={classes.projectGrid}>
         <div className={classes.projectCard}>
           <h3>Smart Helmet</h3>
+          <br />
           <p>
             Developed a smart helmet using Arduino uno r3 which helped the rider
             to alert if he has not worn the helmet and also if the rider is
             drunk.
           </p>
+        </div>
+        <div className={classes.projectCard}>
+          <h3>Mark My Views</h3>
+          <br />
+          <div>
+            <ul>
+              <li>
+                No dedicated website which collectively contains Engineering
+                books of renowned authors and publications.
+              </li>
+              <li>
+                Students need to compare and choose the best book which fulfills
+                their need by manually visiting website of renowned publications
+                one-by-one which eventually is time consuming.
+              </li>
+              <li>
+                This platform will help students to compare and select the best
+                book as per their needs and reviews from other peers which
+                eventually save time.
+              </li>
+              <li>
+                Students can post reviews for books which would help simplifying
+                the process of choosing between many books.
+              </li>
+              <li>
+                Students can segregate books as per the subject or course that
+                they have enrolled in the category section.
+              </li>
+            </ul>
+            <br />
+          </div>
+          <Button
+            href='https://desolate-island-31567.herokuapp.com/'
+            target='__blank'
+            className={classes.btn}
+          >
+            Checkout!
+          </Button>
         </div>
       </div>
     </div>
