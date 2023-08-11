82a43e0a0b234d8317e2683bbb799ad937ad40ad
darshan9405 <darshanb9405@gmail.com>
2023-08-11 10:30:02 +0530

    update info

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,30 +1,100 @@
-import classes from './Projects.module.css'
-import { Button } from 'react-bootstrap'
-import sh from '../../images/sh.png'
+import classes from "./Projects.module.css";
+import { Button } from "react-bootstrap";
+import sh from "../../images/sh.png";
+import elite from "../../images/eilte.png";
+import namma from "../../images/namma.jpg";
+import algo from "../../images/algo.jpeg";
 const Projects = () => {
   return (
-    <div className={classes.projects} id={'projects'}>
-      <h2 style={{ fontSize: '2.5rem' }}>Projects</h2>
+    <div className={classes.projects} id={"projects"}>
+      <h2 style={{ fontSize: "2.5rem" }}>Featured</h2>
+      <br />
+      <div className={classes.projectGrid}>
+        <div className={classes.projectCard}>
+          <h3 style={{ textAlign: "center" }}>
+            Ranked 2nd in Quant Quest organized by Rendezvous 2023 (IIT Delhi)
+          </h3>
+          <div className={classes.imgContainer}>
+            <img
+              className={classes.img}
+              style={{ width: "350px", height: "250px" }}
+              alt={"Not found!"}
+              src={algo}
+            />
+          </div>
+          <Button
+            href="https://www.campustimespune.com/quantquest-iitd-2023/"
+            target="__blank"
+            className={classes.btn}
+          >
+            Checkout!
+          </Button>
+        </div>
+      </div>
+      <br />
+      <h2 style={{ fontSize: "2.5rem" }}>Projects</h2>
       <br />
       <br />
       <div className={classes.projectGrid}>
         <div className={classes.projectCard}>
-          <h3>Smart Helmet</h3>
+          <h3>ELITƎ</h3>
           <br />
           <div className={classes.imgContainer}>
-            <img className={classes.img} alt={'Not found!'} src={sh} />
+            <img
+              className={classes.img}
+              style={{ width: "fit-content", height: "300px" }}
+              alt={"Not found!"}
+              src={elite}
+            />
           </div>
-          <ul>
-            <li>Developed a smart helmet using Arduino uno r3.</li>
-            <li>
-              Motive was to restrict the rider from driving without wearing a
-              Helmet or without him being in conscious condition.
-            </li>
-            <li>
-              Used MQ3 Alcohol Sensor to detect for the consciousness of the
-              rider.
-            </li>
-          </ul>
+          <div>
+            <ul>
+              <li>Designed an interactive and engaging puzzle website.</li>
+              <li>
+                Implemented using Django, HTML, CSS, JavaScript, and Bootstrap
+                with a timer and leaderboard.
+              </li>
+              <li>Used SQL database for user data.</li>
+            </ul>
+            <br />
+          </div>
+          <Button
+            href="https://darshan9405.pythonanywhere.com/"
+            target="__blank"
+            className={classes.btn}
+          >
+            Checkout!
+          </Button>
+        </div>
+        <div className={classes.projectCard}>
+          <h3>APP-LESS RIDE BOOKING</h3>
+          <br />
+          <div className={classes.imgContainer}>
+            <img
+              className={classes.img}
+              style={{ width: "fit-content", height: "300px" }}
+              alt={"Not found!"}
+              src={namma}
+            />
+          </div>
+          <div>
+            <ul>
+              <li>
+                Developed a Telegram bot that offers a hassle‑free booking
+                experience to customers.
+              </li>
+              <li>Developed using Python Telegram Bot library.</li>
+              <li>Received Honorary mentions from Namma Yatri.</li>
+            </ul>
+            <br />
+          </div>
+          <Button
+            href="https://t.me/cyborgs_namma_yatri_bot"
+            target="__blank"
+            className={classes.btn}
+          >
+            Checkout!
+          </Button>
         </div>
         <div className={classes.projectCard}>
           <h3>Mark My Views</h3>
@@ -57,15 +127,33 @@ const Projects = () => {
             <br />
           </div>
           <Button
-            href='https://desolate-island-31567.herokuapp.com/'
-            target='__blank'
+            href="https://markmyviews.onrender.com/"
+            target="__blank"
             className={classes.btn}
           >
             Checkout!
           </Button>
         </div>
+        <div className={classes.projectCard}>
+          <h3>Smart Helmet</h3>
+          <br />
+          <div className={classes.imgContainer}>
+            <img className={classes.img} alt={"Not found!"} src={sh} />
+          </div>
+          <ul>
+            <li>Developed a smart helmet using Arduino uno r3.</li>
+            <li>
+              Motive was to restrict the rider from driving without wearing a
+              Helmet or without him being in conscious condition.
+            </li>
+            <li>
+              Used MQ3 Alcohol Sensor to detect for the consciousness of the
+              rider.
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
-  )
-}
-export default Projects
+  );
+};
+export default Projects;
--- src/components/WorkExperience/WorkExperience.js
+++ src/components/WorkExperience/WorkExperience.js
@@ -1,9 +1,10 @@
-import classes from './WorkExperience.module.css'
-import int from '../../images/internship.png'
+import classes from "./WorkExperience.module.css";
+import int from "../../images/internship.png";
+import stock from "../../images/stock.png";
 const WorkExperience = () => {
   return (
     <div className={classes.projects}>
-      <h2 style={{ fontSize: '2.5rem' }}>Work Experience</h2>
+      <h2 style={{ fontSize: "2.5rem" }}>Work Experience</h2>
       <br />
       <br />
       <div className={classes.projectGrid}>
@@ -13,11 +14,11 @@ const WorkExperience = () => {
           <div className={classes.imgDiv}>
             <a
               href={
-                'https://drive.google.com/file/d/1Sva6ErgSqlxfpD4hxx3ZITYeO_33aF8A/view?usp=sharing'
+                "https://drive.google.com/file/d/1Sva6ErgSqlxfpD4hxx3ZITYeO_33aF8A/view?usp=sharing"
               }
-              target='__blank'
+              target="__blank"
             >
-              <img className={classes.img} alt={'Not found!'} src={int}></img>
+              <img className={classes.img} alt={"Not found!"} src={int}></img>
             </a>
           </div>
           <ul>
@@ -28,8 +29,22 @@ const WorkExperience = () => {
             <li>Used MERN stack for developing recruitment website.</li>
           </ul>
         </div>
+        <div className={classes.projectCard}>
+          <h3>Credenz-Wallstreet</h3>
+          <br />
+          <div className={classes.imgDiv}>
+            <img className={classes.img} alt={"Not found!"} src={stock}></img>
+          </div>
+          <ul>
+            <li>Stock market event organized by PICT IEEE Student Branch.</li>
+            <li>
+              Developed Web‑App using ReactJS(front‑end) and Django(Back‑end).
+            </li>
+            <li>Around 200+ Students played this 3 day long event.</li>
+          </ul>
+        </div>
       </div>
     </div>
-  )
-}
-export default WorkExperience
+  );
+};
+export default WorkExperience;
